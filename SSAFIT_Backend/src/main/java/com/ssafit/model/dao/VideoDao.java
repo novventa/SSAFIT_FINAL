c125a1fd89a7938fb6ee554d34633f8e712f12a7
김용균 <kyg4199@naver.com>
2023-05-16 14:49:27 +0900

    [+]매퍼 및 설정 수정

--- SSAFIT_Backend/src/main/java/com/ssafit/model/dao/VideoDao.java
+++ SSAFIT_Backend/src/main/java/com/ssafit/model/dao/VideoDao.java
@@ -9,13 +9,19 @@ import com.ssafit.model.dto.VideoSearchCondition;
 
 @Repository
 public interface VideoDao {
-	
+
 	public List<Video> selectAll();
+
 	public Video selectOne(int idx);
+
 	public int insert(Video video);
+
 	public int updateViewCnt(int idx);
+
 	public int updateLikeCnt(int idx);
+
 	public int delete(int idx);
+
 	public List<Video> search(VideoSearchCondition condition);
-	
+
 }
